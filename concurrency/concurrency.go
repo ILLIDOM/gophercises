ddeda4499adb5538625ca41401345e38eb0266a9
ILLDOM <illi.dominique.v@gmail.com>
2022-09-29 11:10:08 +0200

    added concurrency pattern and interfaces

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"math/rand"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -55,18 +59,124 @@ func printStuff(msg string, c chan string) {
 	}
 }
 
+// SELECT Pattern - (select to use first channel which is ready)
+func channelSelect(c1 chan string, c2 chan string, c3 chan int) {
+	time.Sleep(1 * time.Millisecond)
+	select {
+	case v1 := <-c1:
+		fmt.Printf("received %v from c1\n", v1)
+	case v2 := <-c2:
+		fmt.Printf("received %v from c2\n", v2)
+	case c3 <- 23:
+		fmt.Printf("sent %v to c3\n", 23)
+	default:
+		fmt.Printf("no one was ready to communicate\n")
+	}
+}
+
+func writeToChannel(c chan string) {
+	c <- "message"
+}
+
+func concurrentDownload(urls []string, result chan string) {
+	for _, url := range urls {
+		go func(url string) {
+			response, err := http.Get(url)
+			if err != nil {
+				result <- fmt.Sprintf("Failed downloading URL: %s\n", url)
+			}
+			defer response.Body.Close()
+			if response.StatusCode != 200 {
+				result <- fmt.Sprintf("Failed downloading URL: %s\n. Non 200 return code!", url)
+			}
+			var data bytes.Buffer
+			_, err = io.Copy(&data, response.Body)
+			if err != nil {
+				result <- fmt.Sprintf("Failed to copy response Body of URL: %s", url)
+			}
+			result <- string(data.Bytes()[:40]) // write first 40 bytes as string into channel
+		}(url)
+	}
+
+}
+
+type Result struct {
+	message string
+	Error   error
+}
+
+func concurrentDownloadCustomResult(urls []string, result chan Result) {
+	for _, url := range urls {
+		go func(url string) {
+			res := Result{}
+			response, err := http.Get(url)
+			if err != nil {
+				res.Error = err
+				result <- res
+				return
+			}
+			defer response.Body.Close()
+			if response.StatusCode != 200 {
+				res.Error = errors.New(fmt.Sprintf("Failed downloading URL: %s\n. Non 200 return code!", url))
+				result <- res
+				return
+			}
+			var data bytes.Buffer
+			_, err = io.Copy(&data, response.Body)
+			if err != nil {
+				res.Error = err
+				result <- res
+			}
+			res.message = string(data.Bytes()[:40])
+			result <- res // write first 40 bytes as string into channel
+		}(url)
+	}
+}
+
 func main() {
-	// synchronization without return value - WaitGroup
+	// // synchronization without return value - WaitGroup
 	// notify("Service-1", "Service-2", "Service-3")
 
-	// synchronization with return values - channels
+	// // synchronization with return values - channels
 	// notifyWithReturnValue("Service-1", "Service-2", "Service-3")
 
-	// communicate between main and a go-routine
-	c := make(chan string)
-	go printStuff("boring!", c)
-	for i := 0; i < 5; i++ { // read 5 strings from channel
-		fmt.Printf("You say: %q\n", <-c) // reading from channel - waits for sender (blocks)
+	// // communicate between main and a go-routine
+	// c := make(chan string)
+	// go printStuff("boring!", c)
+	// for i := 0; i < 5; i++ { // read 5 strings from channel
+	// 	fmt.Printf("You say: %q\n", <-c) // reading from channel - waits for sender (blocks)
+	// }
+	// fmt.Println("Leaving")
+
+	// SELECT PATTERN
+	c1 := make(chan string)
+	c2 := make(chan string)
+	c3 := make(chan int)
+
+	go writeToChannel(c1)
+	go writeToChannel(c2)
+
+	channelSelect(c1, c2, c3)
+
+	// Async Download Simple
+	urls := []string{"https://www.google.com", "https://www.20min.ch", "https://www.blick.ch", "https://start.fedoraproject.org/", ""}
+	// result := make(chan string)
+	// concurrentDownload(urls, result)
+
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Println(<-result)
+	// }
+
+	// Async Download custom response
+	result := make(chan Result)
+	concurrentDownloadCustomResult(urls, result)
+	for i := 0; i < len(urls); i++ {
+		res := <-result
+		if res.message == "" {
+			fmt.Printf("Nothing downloaded due to Error: %v\n", res.Error)
+		} else {
+			fmt.Printf("Downloaded: %s\n", res.message)
+		}
 	}
-	fmt.Println("Leaving")
+
 }
--- datastructures/interfaces/interfaces.go
+++ datastructures/interfaces/interfaces.go
@@ -18,9 +18,22 @@ func (d Demo) Sketch() {
 	fmt.Println("I am sketchin")
 }
 
+type NewDemo struct{}
+
+func (d NewDemo) Draw() {
+	fmt.Println("I am drawing new")
+}
+
+func (d NewDemo) Sketch() {
+	fmt.Println("I am sketching new")
+}
+
 func main() {
 	d := Demo{}        // demo has the methods Draw and Sketch Implemented
 	usingInterfaces(d) // beause Demo implements Draw and Sketch it can be used as a MyInterface
+
+	nD := NewDemo{}
+	usingInterfaces(nD)
 }
 
 func usingInterfaces(mI MyInterface) {
